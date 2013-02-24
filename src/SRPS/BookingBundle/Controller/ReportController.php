2a765b60a220d659658dc7c5c66837280336bb85
Howard Miller <howardsmiller@gmail.com>
2013-02-24 16:35:54 +0000

    adding reports list

---
+++ src/SRPS/BookingBundle/Controller/ReportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SRPS\BookingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ReportController extends Controller
+{
+    public function listAction($serviceid)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $booking = $this->get('srps_booking');
+
+        // Clear session and delete expired purchases
+        $booking->cleanPurchases();
+
+        // Get the service object
+        $service = $em->getRepository('SRPSBookingBundle:Service')
+            ->find($serviceid);
+        if (!$service) {
+            throw new \Exception('Unable to find service');
+        }
+
+        // get the purchases for this service
+        $purchases = $em->getRepository('SRPSBookingBundle:Purchase')
+            ->findBy(array('serviceid'=>$serviceid, 'completed'=>true));
+
+        return $this->render('SRPSBookingBundle:Report:list.html.twig', array(
+            'service' => $service,
+            'purchases' => $purchases,
+        ));
+    }
+
+
+}
+
